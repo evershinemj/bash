beb4bc3583faac78f71155b93e22f0c638ff0533
wangxueming@windows <459226479@qq.com>
2019-09-07 19:37:06 +0800

    modified:   find.sh     about using `xargs` with `find` via a pipe

--- find.sh
+++ find.sh
@@ -8,9 +8,15 @@ find . -name '*.flv' -mtime +1  # modified time is more than one day
 find . -name '*.flv' -ctime +1  # create time is more than one day
 
 # -exec
-find . -name '*.flv' -atime -1 -exec mpv {} \;  # note the trailing syntax '\;'
+## note that -exec handles each result from `find` one by one 
+find . -name '*.flv' -atime -1 -exec mpv {} \;  # note the trailing syntax '\;' # just like mpv foo; mpv bar; ...
 find . -name '*.flv' -atime -1 -exec cp {} ~/foo \;  # note the trailing syntax '\;'
 
 # -ok
 ## note that -ok is similar to -exec except that it asks whether to execute the command
 find . -name '*.flv' -atime -1 -ok mpv {} \;  # note the trailing syntax '\;'
+
+# xargs
+## note that |xargs behaves differently from -exec; it sends all args to the command at once
+find . -name '*.flv' -atime -1 | xargs mpv # just like mpv *; plays all files at once
+
