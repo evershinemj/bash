4c430130a09426b0f9376ee53612ea6d97861f99
wangxueming@windows <459226479@qq.com>
2019-09-22 19:10:49 +0800

    modified:   grep.sh     +# note that | as `or` is ERE(extended regexp) syntax     +ls | grep -i -v 'michael\|owl\|杰伦\|俊杰\|五月天' | less     +ls | egrep -i -v 'michael|owl|杰伦|俊杰|五月天' | less     +ls | grep -E -i -v 'michael|owl|杰伦|俊杰|五月天' | less

--- grep.sh
+++ grep.sh
@@ -1,2 +1,7 @@
 # recursive grep
 grep -r <pattern> <directory>
+
+# note that | as `or` is ERE(extended regexp) syntax
+ls | grep -i -v 'michael\|owl\|杰伦\|俊杰\|五月天' | less
+ls | egrep -i -v 'michael|owl|杰伦|俊杰|五月天' | less
+ls | grep -E -i -v 'michael|owl|杰伦|俊杰|五月天' | less
